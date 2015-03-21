7233ee67063b8b0451db3fc6de3ca8ed015111a5
Eugene Rozenfeld <erozen@microsoft.com>
2015-03-21 00:14:48 -0700

    Revert "Enable calls to methods that require type arg (instantiation information)"

--- lib/Reader/readerir.cpp
+++ lib/Reader/readerir.cpp
@@ -1331,9 +1331,11 @@ FunctionType *GenIR::getFunctionType(CORINFO_SIG_INFO &Sig,
   bool HasTypeArg = Sig.hasTypeArg();
 
   if (HasTypeArg) {
-    CORINFO_CLASS_HANDLE Class = 0;
-    Type *TypeArgType = getType(CORINFO_TYPE_NATIVEINT, Class);
-
+    // maybe not the right type... for now just match what we pick in
+    // ReaderBase::buildUpParams
+    CORINFO_CLASS_HANDLE Class =
+        getBuiltinClass(CorInfoClassId::CLASSID_TYPE_HANDLE);
+    Type *TypeArgType = getType(CORINFO_TYPE_PTR, Class);
     Arguments.push_back(TypeArgType);
   }
 
@@ -3190,6 +3192,10 @@ IRNode *GenIR::genCall(ReaderCallTargetData *CallTargetInfo,
     if (!CallTargetInfo->isUnmarkedTailCall()) {
       throw NotYetImplementedException("Tail call");
     }
+  }
+
+  if (SigInfo->hasTypeArg()) {
+    throw NotYetImplementedException("Call HasTypeArg");
   }
 
   if ((CallInfo != nullptr) && (CallInfo->kind == CORINFO_VIRTUALCALL_STUB)) {
